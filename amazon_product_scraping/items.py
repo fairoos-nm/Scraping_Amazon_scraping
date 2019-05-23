c7cacdc6f625a4a79328504ef5e82d5c6d4ffb3f
fairoos <fairoosnm123@gmail.com>
2019-05-23 10:10:38 +0530

    Added items

--- amazon_product_scraping/items.py
+++ amazon_product_scraping/items.py
@@ -10,5 +10,7 @@ import scrapy
 
 class AmazonProductScrapingItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    product_name = scrapy.Field()
+    product_author = scrapy.Field()
+    product_price = scrapy.Field()
+    product_link = scrapy.Field()
